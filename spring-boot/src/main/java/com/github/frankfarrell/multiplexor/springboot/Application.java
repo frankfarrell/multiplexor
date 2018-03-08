871738c909a69c70fc1d69856f39a8a589fb32f6
frankfarrell <ofearghp@gmail.com>
2018-03-08 10:48:11 +0000

    chore: ObjectMapper handles jdk8 features

--- spring-boot/src/main/java/com/github/frankfarrell/multiplexor/springboot/Application.java
+++ spring-boot/src/main/java/com/github/frankfarrell/multiplexor/springboot/Application.java
@@ -1,6 +1,11 @@
 package com.github.frankfarrell.multiplexor.springboot;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.github.frankfarrell.multiplexor.springboot.servlet.MultiplexorServlet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import java.util.HashMap;
@@ -54,7 +60,16 @@ public class Application {
 
 
     @Bean
-    public ObjectMapper objectMapper(){
-        return new ObjectMapper();
+    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+        return Jackson2ObjectMapperBuilder.json()
+                .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
+                        DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .modules(jdk8Module());
+    }
+
+    @Bean
+    public Module jdk8Module() {
+        return new Jdk8Module();
     }
 }
